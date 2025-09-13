3f7beb5d11be1ce260d0ecf6bf069cf34f6583d2
Daniel Unsworth <danielunsworth@Daniels-MacBook-Pro.local>
2025-09-13 09:24:21 +0100

    update docker compose

--- lwm.client/vite.config.ts
+++ lwm.client/vite.config.ts
@@ -6,12 +6,10 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 
-/*const baseFolder =
+const baseFolder =
     process.env.APPDATA !== undefined && process.env.APPDATA !== ''
         ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;*/
-
- const baseFolder = "/https"
+        : `${process.env.HOME}/.aspnet/https`;
 
  const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
  const certificateName = certificateArg ? certificateArg.groups.value : "lwm.client";
