854ed2a65e3299c1c4e06d3a209edf7a6ed6638f
Daniel Unsworth <danielunsworth@Daniels-MacBook-Pro.local>
2025-09-13 09:45:52 +0100

    update vite https dir

--- lwm.client/vite.config.ts
+++ lwm.client/vite.config.ts
@@ -6,10 +6,12 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
+// const baseFolder =
+//     process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+//         ? `${process.env.APPDATA}/ASP.NET/https`
+//         : `${process.env.HOME}/.aspnet/https`;
+
+const baseFolder = '/https';
 
  const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
  const certificateName = certificateArg ? certificateArg.groups.value : "lwm.client";
