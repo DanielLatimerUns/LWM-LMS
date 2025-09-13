401a4fd48ca05972b289087a8849e824c49cd93b
Daniel Unsworth <danielunsworth@Daniels-MacBook-Pro.local>
2025-09-13 20:22:36 +0100

    update docker deploy script

--- LWM.Server/Deployment/Scripts/DeployDocker.sh
+++ LWM.Server/Deployment/Scripts/DeployDocker.sh
@@ -3,8 +3,8 @@ cd ~/lwm/LWM-LMS || exit
 git checkout master
 git pull
 echo "************** Starting Build ***************"
-sudo docker compose down
-sudo docker build
-echo "************** Finished Build ***************"
-sudo docker up
-echo "************** Docker started ***************"
+docker compose down
+docker build
+"************** Finished Build ***************"
+docker up
+"************** Docker started ***************"
