b22f0dfb9921c5f27d16ec5075ae217ad50357b6
Daniel Unsworth <danielunsworth@Daniels-MacBook-Pro.local>
2025-09-14 10:39:52 +0100

    move build script to root

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -1,10 +1,9 @@
 echo "************** Pulling from master ***************"
-#cd ~/lwm/LWM-LMS || exit
 git checkout master || exit
 git pull || exit
 echo "************** Starting Build ***************"
 docker compose down || exit
 docker compose build || exit
 "************** Finished Build ***************"
-docker compose up
+docker compose up -d
 "************** Docker started ***************"
